4df842d06402c0b025cf501d70c523498359c9dd
ErickJoestar <erickborquez@gmail.com>
2020-07-09 20:22:11 -0600

    fix app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fyxh4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
-  .then(() => app.listen(process.end.PORT || 5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => {
     console.log(err);
   });
